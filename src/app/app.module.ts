cb91a346445f65cf0418fffb08400de67b0c0db4
Mathis Jungers <mathis.jungers@edu.technobel.be>
2023-04-03 23:32:54 +0200

    ajustement 03/04

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SujetComponent } from './components/sujet/sujet.component';
-import {RouterOutlet} from "@angular/router";
+
 import {AppRoutingModule} from "./app-routing.modules";
 import { HttpClientModule} from "@angular/common/http";
 import { SujetCreateComponent } from './components/sujet-create/sujet-create.component';
@@ -11,16 +11,7 @@ import {ReactiveFormsModule} from "@angular/forms";
 import { SujetDetailsComponent } from './components/sujet-details/sujet-details.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {
-  NbActionsModule,
-  NbContextMenuModule,
-  NbFormFieldModule,
-  NbInputModule,
-  NbLayoutModule,
-  NbThemeModule
-} from "@nebular/theme";
-import {NbEvaIconsModule} from "@nebular/eva-icons";
+
 
 @NgModule({
   declarations: [
